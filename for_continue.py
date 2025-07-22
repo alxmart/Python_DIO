76240fa2dd1c09e2128fec28b3b780f4652a1b2f
alxmart <alxmartinez@gmail.com>
2025-07-22 12:58:20 -0300

    For com continue.

---
+++ for_continue.py
@@ -0,0 +1,11 @@
+
+for num in range(100):
+    if num % 2 == 0:
+        #print(num, end=" ")
+        if num == 12:
+            continue
+        print(num)
+    else:
+        print(f"{num} é ímpar\n", end=" ")
+print()
+
