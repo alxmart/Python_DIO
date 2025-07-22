a8498928ef6c3baa61355c1d6259f2cd3f07a068
alxmart <alxmartinez@gmail.com>
2025-07-22 08:56:55 -0300

    Ajustando saída com casa decimais

--- identacao_blocos.py
+++ identacao_blocos.py
@@ -4,16 +4,16 @@ def sacar(valor: float) -> None:
     saldo = 9500.00
 
     if saldo >= valor:
-        print("Valor sacado: ", float(valor))
+        print("Valor sacado: R$ %.2f" % valor)
         saldo -= valor
-        print("Saldo: ", float(saldo))
+        print("Saldo: R$ %.2f" % saldo)
     else:
         print("Saldo insuficiente para saque")
-        print("Saldo: ", float(saldo))
-        print("Valor do saque: ", float(valor))
+        print("Saldo: R$ %.2f"  %saldo)
+        print("Valor do saque: R$ %.2f"  %valor)
         print("Operação não realizada")
 
-    print("Obrigado por usar o nosso banco")
+    print("Obrigado por usar o nosso banco\n")
 
 
 sacar(1500.00)
