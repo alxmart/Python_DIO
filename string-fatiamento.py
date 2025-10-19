ce856d385255460621fd7cdfd2cd9e463a9df95b
alxmart <alxmartinez@gmail.com>
2025-10-19 14:58:20 -0300

    Strings / Fatiamento: separando as liinhas

--- string-fatiamento.py
+++ string-fatiamento.py
@@ -1,5 +1,7 @@
+
 # FATIAMENTO DE STRINGS:
 nome2 = "Bart Simpson"
+
 print(nome2[0:4])  # Bart
 
 print(nome2[:4])   # Bart
@@ -16,49 +18,90 @@ print(nome2[1::2]) # atSimson
 
 print(nome2[::-1]) # nospmiS traB
 
+
 nome = "Guilherme Arthur de Carvalho"
 
 print(nome[0])  # G
-print(nome[-1]) 
+
+print(nome[-1]) # o
+
 print(nome[:9])  # Guilherme
+
 print(nome[10:])  # Arthur de Carvalho
+
 print(nome[10:16]) # Arthur
+
 print(nome[10:16:2]) # Atru
+
 print(nome[:]) # Guilherme Arthur de Carvalho
+
 print(nome[::-1]) # ovlahraC ed ruhtrA emireliuG
 
 print(nome[::3])  # GlmeAh edealvo 
+
 print(nome[5:15:2])  # hr eCrl 
+
 print(nome[-1:-10:-1])  # olrahC ed ruhtrA emireliuG
+
 print(nome[::4])  # Guhr eahv
+
 print(nome[2:20:5])  # i r d a
+
 print(nome[::-2])  # olha ed rhtA emireliuG
+
 print(nome[10::-1])  # emireliuG
+
 print(nome[-5::-3])  # l rAuh ieu
+
 print(nome[4:0:-1])  # liruiG
+
 print(nome[-1:-21:-4])  # o a rG
+
 print(nome[3:15:4])  # lmeA
+
 print(nome[-3::-2])  # v a h r u iG
+
 print(nome[::6])  # G r a
+
 print(nome[1:18:3])  # i r d a
+
 print(nome[-4::-1])  # olrahC ed ruhtrA emireliuG
+
 print(nome[5:25:5])  # h e a o
+
 print(nome[-2::-4])  # h r iG
+
 print(nome[::5])  # G r a
+
 print(nome[7:0:-2])  # u l i
+
 print(nome[-6::-3])  # a r i
+
 print(nome[12:4:-1])  # mrehli
+
 print(nome[-1:-16:-2])  # o a r h d rA
+
 print(nome[::7])  # G d a
+
 print(nome[15:5:-3])  # A e l
+
 print(nome[-3::-4])  # v r u
+
 print(nome[4:25:6])  # h r o
+
 print(nome[-5::-2])  # l a h r iG
+
 print(nome[3:20:5])  # l e a
+
 print(nome[::-3])  # o a r h d rA emireliuG
+
 print(nome[6::-1])  # emireliuG
+
 print(nome[-2::-4])  # h r iG
+
 print(nome[10:0:-2])  # i l u
+
 print(nome[-7::-3])  # s a h r iG
+
 print(nome[2:18:4])  # l e a
 
