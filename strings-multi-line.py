5676c8e64b64f90f8f06ccd3cee536700fe99483
alxmart <alxmartinez@gmail.com>
2025-10-19 15:42:00 -0300

    Multi line strings exercises

---
+++ strings-multi-line.py
@@ -0,0 +1,31 @@
+
+nome = "Guilherme"
+
+mensagem = f"""
+  Olá, meu nome é {nome},
+Eu estou aprendendo Python.
+"""
+print(mensagem)
+
+#--------------------------------------------------------------------------------------
+
+mensagem2 = f'''
+  Olá, meu nome é {nome},
+Eu estou aprendendo Python.
+'''
+
+print(mensagem2)
+
+#--------------------------------------------------------------------------------------
+
+print(
+    """
+========= MENU =========
+
+1 - Depositar
+2 - Sacar
+0 - Sair
+
+=========================
+    """
+)
