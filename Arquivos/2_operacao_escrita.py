3e413e40109e1b0638b0a236b42918c77f917dea
alxmart <alxmartinez@gmail.com>
2025-12-22 18:29:41 -0300

    Exercícios com Arquivos

--- Arquivos/2_operacao_escrita.py
+++ Arquivos/2_operacao_escrita.py
@@ -1,6 +1,8 @@
-arquivo = open(
-    "/home/guilherme/Projetos/dio/codigo-fonte/trilha-python-dio/05 - Manipulação de arquivos/teste.txt", "w"
-)
+
+arquivo = open("/Users/alemart/PythonProjects/Python_DIO/Arquivos/teste2.txt", "w")
+
 arquivo.write("Escrevendo dados em um novo arquivo.")
+
 arquivo.writelines(["\n", "escrevendo", "\n", "um", "\n", "novo", "\n", "texto"])
+
 arquivo.close()
